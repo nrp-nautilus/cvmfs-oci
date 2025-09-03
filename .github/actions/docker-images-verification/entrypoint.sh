b9c7bb5c36514f2003f9cc455fd4a0776d2cf94c
hzhu16 <huijun.zhu@unl.edu>
2025-09-03 16:26:04 -0500

    Update entrypoint.sh

--- .github/actions/docker-images-verification/entrypoint.sh
+++ .github/actions/docker-images-verification/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-python3 oci-cvmfs.py . https://raw.githubusercontent.com/nrp-nautilus/cvmfs-oci/refs/heads/main/images.txt --verify
+python3 oci-cvmfs.py . https://raw.githubusercontent.com/nrp-nautilus/cvmfs-oci/refs/$GITHUB_REF/main/images.txt --verify
